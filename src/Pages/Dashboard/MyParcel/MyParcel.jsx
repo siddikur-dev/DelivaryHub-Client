c765ef320e9954d306f8621652b20aed5383371a
siddikur-dev <siddikur.dev@gmail.com>
2025-11-20 21:59:15 +0600

    Added: Delete functionality implement & window auto refresh refetch() transtack  in 'MyParcel Dashboard' page

--- src/Pages/Dashboard/MyParcel/MyParcel.jsx
+++ src/Pages/Dashboard/MyParcel/MyParcel.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import useAuth from "../../../hooks/useAuth";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import { FaTrashAlt } from "react-icons/fa";
 
 const MyParcel = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch } = useQuery({
     queryKey: ["my-parcels", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/parcels?email=${user?.email}`);
@@ -14,10 +16,35 @@ const MyParcel = () => {
     },
   });
 
+  // const handle delete parcel
+  const handleDeleteParcel = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/parcels/${id}`).then((data) => {
+          if (data.data.deletedCount) {
+            // it will be automatically refresh window
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your parcel has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <div className="overflow-x-auto shadow-md rounded-xl">
       <table className="table table-zebra w-full">
-
         <thead className="bg-base-200 text-base font-semibold">
           <tr>
             <th>#</th>
@@ -49,11 +76,18 @@ const MyParcel = () => {
                 </span>
               </td>
               <td className="space-x-2">
-                <button className="btn btn-xs btn-outline">View</button>
+                <button className="btn btn-xs hover:btn-secondary text-black">
+                  Details
+                </button>
                 {parcel.payment_status === "unpaid" && (
                   <button className="btn btn-xs btn-primary">Pay</button>
                 )}
-                <button className="btn btn-xs btn-error">Delete</button>
+                <button
+                  onClick={() => handleDeleteParcel(parcel._id)}
+                  className="btn btn-xs btn-error btn-outline"
+                >
+                  <FaTrashAlt />
+                </button>
               </td>
             </tr>
           ))}
