4a4eb3388193788e456b7cdfc666d131422a25bf
siddikur-dev <siddikur.dev@gmail.com>
2025-11-27 07:44:24 +0600

    Added: Toggle user Role Make Admin and Remove from admin 'user managament page'

--- src/Pages/Dashboard/UserManagement/UserManagement.jsx
+++ src/Pages/Dashboard/UserManagement/UserManagement.jsx
@@ -1,17 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-
+import { FaUserShield } from "react-icons/fa";
+import { GoShieldX } from "react-icons/go";
+import Swal from "sweetalert2";
 const UserManagement = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: users = [] } = useQuery({
+  // const [role, setRole] = useState()
+  const { data: users = [], refetch } = useQuery({
     queryKey: "users",
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
       return res.data;
     },
   });
-  
+
+  const handleMakeAdmin = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: `You want to make ${user.displayName} an admin?`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, make admin!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const roleInfo = { role: "admin" };
+        axiosSecure.patch(`/users/${user._id}`, roleInfo)
+          .then(res => {
+            if (res.data.modifiedCount) {
+              refetch()
+              Swal.fire({
+                title: "Success!",
+                text: "User role has been updated to admin.",
+                icon: "success"
+              });
+            }
+          })
+          .catch(error => {
+            Swal.fire({
+              title: "Error!",
+              text: "Failed to update user role.",
+              icon: "error"
+            });
+          });
+      }
+    });
+  }
+
+  const handleRemoveAdmin = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: `You want to make ${user.displayName} a user?`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, make user!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const roleInfo = { role: "user" };
+        axiosSecure.patch(`/users/${user._id}`, roleInfo)
+          .then(res => {
+            if (res.data.modifiedCount) {
+              refetch()
+              Swal.fire({
+                title: "Success!",
+                text: "Admin role has been updated to user.",
+                icon: "success"
+              });
+            }
+          })
+          .catch(error => {
+            Swal.fire({
+              title: "Error!",
+              text: "Failed to update user role.",
+              icon: "error"
+            });
+          });
+      }
+    });
+  }
+
+
   return (
     <div>
       <table className="table table-zebra w-full">
@@ -22,6 +94,7 @@ const UserManagement = () => {
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
+            <th>Action Button</th>
           </tr>
         </thead>
         <tbody>
@@ -39,7 +112,28 @@ const UserManagement = () => {
                 <td>{p.displayName}</td>
                 <td>{p.email}</td>
                 <td>{p.role}</td>
-               
+                <td>
+                  <>{
+                    p.role === "user"
+                      ?
+                      <button
+                        onClick={() => handleMakeAdmin(p)}
+                        className="btn btn-xs btn-secondary text-white  "
+                      >
+                        <FaUserShield />
+
+                      </button>
+                      :
+                      <button
+                        onClick={() => handleRemoveAdmin(p)}
+                        className="btn btn-xs btn-primary"
+                      >
+                        <GoShieldX />
+
+                      </button>
+                  }
+                  </>
+                </td>
               </tr>
             ))
           ) : (
